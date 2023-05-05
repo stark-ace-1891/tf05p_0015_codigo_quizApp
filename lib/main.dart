9de0079fbd2386403534a778949f5cc367067ef5
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-05 16:30:29 -0500

    Definiendo estructura inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());  
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Prueba"),
-        ),
-        body: Text("Prueba"),
+    return MaterialApp(home: QuizPage());
+  }
+}
+
+class QuizPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QuizApp"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {},
+              child: Text(
+                "Verdadero",
+              ),
+              color: Colors.greenAccent,
+            ),
+          ),
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {},
+              child: Text(
+                "Verdadero",
+              ),
+              color: Colors.redAccent,
+            ),
+          ),
+        ],
       ),
     );
   }
